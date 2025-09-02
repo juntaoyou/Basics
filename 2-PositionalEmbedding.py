8cef2aa3a0496e24b55b018ceac7a666279afcfd
Juntaoyou <juntaoyou05@163.com>
2025-09-02 22:30:56 +0800

    First Day Modified1

--- 2-PositionalEmbedding.py
+++ 2-PositionalEmbedding.py
@@ -6,5 +6,7 @@ class PositionalEmbedding(nn.Module):
     def __init__(self):
         super(PositionalEmbedding,self).__init__()
 
-
+    def forward(self, d_model):
+        pass
+    
 # https://www.doubao.com/thread/w22265f79e955ae68

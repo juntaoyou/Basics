e751071b5373b5e121faefa69c9ab3c6e9716d6a
Juntaoyou <juntaoyou05@163.com>
2025-09-03 18:06:46 +0800

    2025/09/03 BatchNorm commit

---
+++ 3-Normalization.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BatchNormalization(nn.Module):
+    def __init__(self):
+        super(BatchNormalization, self).__init__()
+
+# https://www.doubao.com/thread/w3a24bd2b7b7cbf54
+
+
